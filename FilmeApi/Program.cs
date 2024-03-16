61e3d7ee32b44b60a897cdda989ff8799070c2d8
Fernando Barros <fernandes.nando@outlook.com>
2024-03-16 12:42:17 -0300

    Criando uma mini POC pra funcionamento da API e instalando as libs.

--- FilmeApi/Program.cs
+++ FilmeApi/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using ServiceImpl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IProcessarVideoService, ProcessarVideoService>();
 
 var app = builder.Build();
 
@@ -16,29 +22,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/filme/enviar", async ([FromBody] string name) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    
+    var service = app.Services.GetService<IProcessarVideoService>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var result = await service.ProcessarVideo(name);
+
+    return Results.Ok(result);
+});
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- ServiceImpl/ProcessarVideoService.cs
+++ ServiceImpl/ProcessarVideoService.cs
@@ -4,5 +4,9 @@ namespace ServiceImpl
 {
     public class ProcessarVideoService : IProcessarVideoService
     {
+        public async Task<string> ProcessarVideo(string videoId)
+        {
+            return $"Sucesso com o {videoId}";
+        }
     }
 }
